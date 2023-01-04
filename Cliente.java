b6760023f6a65e04286cc5134be8547a82f1aab2
wq-prog <77200986+wq-prog@users.noreply.github.com>
2023-01-04 17:40:11 +0000

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -3,12 +3,14 @@ public class Cliente
     private int nif;
     private String nome;
     private String endereco;
-
+    
+    private List<Servico> lista;
     public Cliente(int nif, String n, String e)
     {
         this.nif = nif;
         this.nome = n;
         this.endereco = e;
+        this.lista = new ArrayList<Servico>();
     }
 
     public int getNIF()
@@ -29,8 +31,20 @@ public class Cliente
     public void setEndereco(String e)
     {   this.endereco = e;}
     
+    public List<Servico> getLista(){
+        ArrayList<Servico> temp = new ArrayList<Servico>();
+        for(Servico s:this.servicos)
+            temp.add(s);
+        
+        return temp;
+    }
     public Cliente clone()
-    {   return new Cliente(this.getNIF(), this.getNome(), this.getEndereco());}
+    {   Cliente c = new Cliente(this.getNIF(), this.getNome(), this.getEndereco());
+        for(Servico s:this.lista)
+            c.add(s);
+     
+        return c;
+    }
     
     public boolean equals(Cliente c)
     {
